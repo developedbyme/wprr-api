c3724308e07be4ea378a30dd7a3f1797fbdb1d1f
Marco Hyyryläinen <marco@wheresmar.co>
2017-03-02 09:19:57 +0100

    Output lang in post data

--- libs/MRouterData/MRouterDataEncoder.php
+++ libs/MRouterData/MRouterDataEncoder.php
@@ -8,7 +8,7 @@
 
 	// \MRouterData\MRouterDataEncoder
 	class MRouterDataEncoder {
-		
+
 		protected function _get_meta_data() {
 			$returnObject = array();
 
@@ -37,16 +37,16 @@
 			$current_post_data["title"] = get_the_title($post_id);
 			$current_post_data["excerpt"] = apply_filters('the_excerpt', $post->post_excerpt);
 			$current_post_data["content"] = apply_filters('the_content', $post->post_content);
-			
+
 			$current_post_data["parent"] = $this->encode_post_link($post->post_parent);
-			
+
 			$children_ids = get_posts(array(
 				'post_type' => 'any',
 				'post_parent' => $post_id,
 				'posts_per_page' => -1,
 				'fields' => 'ids'
 			));
-			
+
 			$children = array();
 			foreach($children_ids as $child_id) {
 				$children[] = $this->encode_post_link($child_id);
@@ -68,19 +68,25 @@
 			$current_post_data["author"] = $this->encode_user($author);
 
 			$current_post_data["meta"] = get_post_meta($post_id);
-			
+
 			$current_post_data["acf"] = null;
 			$fields_object = get_field_objects($post_id);
 			if($fields_object !== false) {
-				
+
 				$acf_object = array();
 				foreach($fields_object as $name => $field_object) {
 					$acf_object[$name] = $this->encode_acf_field($field_object, $post_id);
 				}
-				
+
 				$current_post_data["acf"] = $acf_object;
 			}
-			
+
+			$current_post_data["languages"] = null;
+			if ( function_exists('icl_get_languages') ) {
+        $current_post_data["languages"] = icl_get_languages( "skip_missing=0" );
+				$current_post_data["languages"]["ICL_LANGUAGE_CODE"] = ( ICL_LANGUAGE_CODE == "sv" ? "en" : "sv" );
+			}
+
 			$taxonomies = array_keys(get_the_taxonomies($post_id));
 			$term_data_array = array();
 			foreach($taxonomies as $taxonomy) {
@@ -97,10 +103,10 @@
 
 			return $current_post_data;
 		}
-		
+
 		public function encode_image($media_post) {
 			//var_dump($media_post);
-			
+
 			$media_post_id = $media_post->ID;
 			$media_meta = get_post_meta($media_post_id, '_wp_attachment_metadata', true);
 			$sizes = $media_meta['sizes'];
@@ -110,7 +116,7 @@
 			$image_data['id'] = $media_post_id;
 			$image_data['title'] = get_the_title($media_post_id);
 			$image_data['permalink'] = get_permalink($media_post_id);
-			
+
 			$image_data['alt'] = get_post_meta($media_post_id, '_wp_attachment_image_alt', true);
 			$image_data['caption'] = $media_post->post_excerpt;
 			$image_data['description'] = $media_post->post_content;
@@ -123,7 +129,7 @@
 					$image_size_data[$size_name] = array('url' => $image_url_and_size[0], 'width' => $image_url_and_size[1], 'height' => $image_url_and_size[2]);
 				}
 			}
-			
+
 			$image_url_and_size = wp_get_attachment_image_src($media_post_id, 'full');
 			$image_size_data['full'] = array('url' => $image_url_and_size[0], 'width' => $image_url_and_size[1], 'height' => $image_url_and_size[2]);
 
@@ -131,30 +137,30 @@
 
 			return $image_data;
 		}
-		
+
 		public function encode_file($media_post) {
-			
+
 			$media_post_id = $media_post->ID;
 
 			$image_data = array();
 
 			$image_data['id'] = $media_post_id;
 			$image_data['title'] = get_the_title($media_post_id);
-			
+
 			$image_data['alt'] = get_post_meta($media_post_id, '_wp_attachment_image_alt', true);
 			$image_data['caption'] = $media_post->post_excerpt;
 			$image_data['description'] = $media_post->post_content;
-			
+
 			$image_data['url'] = wp_get_attachment_url($media_post_id);
-			
+
 			$preview_url = wp_get_attachment_thumb_url($media_post_id);
-			
+
 			$image_data['previewUrl'] = $preview_url ? $preview_url : null;
 			$image_data['mimeType'] = $media_post->post_mime_type;
-			
+
 			return $image_data;
 		}
-		
+
 		protected function _encode_acf_single_post_object_or_id($post_or_id) {
 			if($post_or_id instanceof \WP_Post) {
 				return $this->encode_post_link($post_or_id->ID);
@@ -163,16 +169,16 @@
 				return $this->encode_post_link($post_or_id);
 			}
 		}
-		
+
 		protected function _encode_acf_post_object($value) {
 			if($value === false) {
 				return null;
 			}
-			
+
 			$return_array = array();
-			
+
 			if(is_array($value)) {
-				
+
 				foreach($value as $post_or_id) {
 					$return_array[] = $this->_encode_acf_single_post_object_or_id($post_or_id);
 				}
@@ -180,10 +186,10 @@
 			else {
 				$return_array[] = $this->_encode_acf_single_post_object_or_id($value);
 			}
-			
+
 			return $return_array;
 		}
-		
+
 		protected function _encode_acf_single_image_or_id($post_or_id) {
 			if($post_or_id instanceof \WP_Post) {
 				return $this->encode_image($post_or_id);
@@ -192,17 +198,17 @@
 				return $this->encode_image(get_post($post_or_id));
 			}
 		}
-		
+
 		protected function _encode_acf_image($value) {
-			
+
 			if($value === false || $value === null) {
 				return null;
 			}
-			
+
 			$return_array = array();
-			
+
 			if(is_array($value)) {
-				
+
 				foreach($value as $post_or_id) {
 					$return_array[] = $this->_encode_acf_single_image_or_id($post_or_id);
 				}
@@ -210,10 +216,10 @@
 			else {
 				$return_array[] = $this->_encode_acf_single_image_or_id($value);
 			}
-			
+
 			return $return_array;
 		}
-		
+
 		protected function _encode_acf_single_taxonomy_or_id($term_or_id, $taxonomy) {
 			if($term_or_id instanceof \WP_Term) {
 				return $this->encode_term($term_or_id);
@@ -222,17 +228,17 @@
 				return $this->encode_term(get_term_by('id', $term_or_id, $taxonomy));
 			}
 		}
-		
+
 		protected function _encode_acf_taxonomy($value, $taxonomy) {
-			
+
 			if($value === false || $value === null) {
 				return null;
 			}
-			
+
 			$return_array = array();
-			
+
 			if(is_array($value)) {
-				
+
 				foreach($value as $id) {
 					$return_array[] = $this->_encode_acf_single_taxonomy_or_id($id, $taxonomy);
 				}
@@ -240,42 +246,42 @@
 			else {
 				$return_array[] = $this->_encode_acf_single_taxonomy_or_id($value, $taxonomy);
 			}
-			
+
 			return $return_array;
 		}
-		
+
 		public function encode_acf_field($field, $post_id, $override_value = null) {
 			//echo('encode_acf_field');
 			//var_dump($field);
-			
+
 			$return_object = array();
-			
+
 			$type = $field['type'];
 			$return_object['type'] = $type;
-			
+
 			$field_value = $override_value ? $override_value : $field['value'];
-			
+
 			switch($type) {
 				case 'repeater':
 					$rows_array = array();
 					$current_key = $field['key'];
 					if(have_rows($current_key, $post_id)) {
 						while(have_rows($current_key, $post_id)) {
-							
+
 							the_row();
 							$current_row = get_row();
-							
+
 							$row_result = array();
-							
+
 							foreach($current_row as $key => $value) {
 								$current_row_field = get_field_object($key, $post_id, false, true);
 								$row_result[$current_row_field['name']] = $this->encode_acf_field($current_row_field, $post_id, $value);
 							}
-							
+
 							array_push($rows_array, $row_result);
 						}
 					}
-					
+
 					$return_object['value'] = $rows_array;
 					break;
 				case 'image':
@@ -322,14 +328,14 @@
 					$return_object['value'] = $field_value;
 					break;
 			}
-			
+
 			return $return_object;
 		}
-		
+
 		public function encode_post_link($post_id) {
 			//echo('encode_post_link');
 			//var_dump($post_id);
-			
+
 			if($post_id === 0 || get_post_status($post_id) !== "publish") {
 				return null;
 			}
@@ -337,7 +343,7 @@
 			$current_post_data["id"] = $post_id;
 			$current_post_data["permalink"] = get_permalink($post_id);
 			$current_post_data["title"] = get_the_title($post_id);
-			
+
 			return $current_post_data;
 		}
 
@@ -372,10 +378,10 @@
 
 			return $return_object;
 		}
-		
+
 		public function encode() {
 			ob_start();
-			
+
 			try {
 
 				$debug = false;
@@ -439,18 +445,18 @@
 				$template_selection['taxonomy'] = ($queried_object instanceof \WP_Term) ? $queried_object->taxonomy : null;
 
 				$data['data']['templateSelection'] = $template_selection;
-			
+
 			}
 			catch(Exception $error) {
 				var_dump($error);
 				$data['metadata']['phpError'] = $error;
 			}
-			
+
 			$php_output = ob_get_contents();
 			ob_clean();
-			
+
 			$data['metadata']['phpOutput'] = $php_output;
-			
+
 			return $data;
 		}
 

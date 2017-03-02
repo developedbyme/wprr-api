7f85e4d1dc008744a9d9bdef66499c221cfc85e4
Marco Hyyryläinen <marco@wheresmar.co>
2017-03-02 18:22:09 +0100

    Added support for frontpage prio selector for news and cases

--- libs/MRouterData/RestApi/PostCaseEndPoint.php
+++ libs/MRouterData/RestApi/PostCaseEndPoint.php
@@ -15,7 +15,22 @@
 			//echo("\OddCore\RestApi\PostCaseEndPoint::perform_call<br />");
 
 			$post_type = $data['post_type'];
+			$post_cases = array();
 
+			// Get the prio items from frontpage ACF
+			$frontpage = get_option('page_on_front');
+			if (get_field('frontpage_news_priority_output', $frontpage)) {
+				$frontpage_news_prio = get_field('frontpage_news_priority_output', $frontpage);
+				$frontpage_news_prio_ids = array_map(function($e) {
+				    return is_object($e) ? $e->ID : $e['ID'];
+				}, $frontpage_news_prio);
+
+				$post_cases = array_merge($post_cases, $frontpage_news_prio_ids);
+			} else {
+				$frontpage_news_prio_ids = array();
+			}
+
+			// Get normal posts and cases
 			$query_args = array(
 				'post_type' => ['post', 'expohouse_case'],
 				'posts_per_page' => -1,
@@ -23,6 +38,9 @@
 			);
 
 			$posts = get_posts($query_args);
+			$posts = array_merge($post_cases, $posts);
+
+			$posts = array_unique($posts);
 
 			foreach ($posts as $post_key => $post) {
 				$categories = get_the_category($post);

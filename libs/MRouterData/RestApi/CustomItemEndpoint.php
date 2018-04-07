cd298a36087cc679876fb861c536e3162196dbf1
Mattias Ekendahl <mattias@developedbyme.com>
2018-04-07 11:17:55 +0200

    Update: added custom item endpoint

---
+++ libs/MRouterData/RestApi/CustomItemEndpoint.php
@@ -0,0 +1,49 @@
+<?php
+	namespace MRouterData\RestApi;
+
+	use \WP_Query;
+	use \MRouterData\OddCore\RestApi\EndPoint as EndPoint;
+
+	// \MRouterData\RestApi\CustomItemEndpoint
+	class CustomItemEndpoint extends EndPoint {
+
+		function __construct() {
+			//echo("\OddCore\RestApi\CustomItemEndpoint::__construct<br />");
+
+
+		}
+
+		public function perform_call($data) {
+			//echo("\OddCore\RestApi\CustomItemEndpoint::perform_call<br />");
+
+			$item_type = $data['item_type'];
+			$id = $data['id'];
+
+			$has_permission_filter_name = M_ROUTER_DATA_DOMAIN.'/custom_item_has_permission_'.$item_type;
+			$query_filter_name = M_ROUTER_DATA_DOMAIN.'/custom_item_get_'.$item_type;
+			$encode_filter_name = M_ROUTER_DATA_DOMAIN.'/custom_range_encode_'.$item_type;
+
+
+			if(!has_filter($query_filter_name)) {
+				return $this->output_error('No custom items for type '.$item_type);
+			}
+
+			$has_permission = apply_filters($has_permission_filter_name, true, $data);
+			if(!$has_permission) {
+				return $this->output_error('Access denied');
+			}
+			
+			do_action(M_ROUTER_DATA_DOMAIN.'/prepare_api_request', $data);
+			
+			$item = apply_filters($query_filter_name, null, $id, $data);
+			
+			$encoded_item = apply_filters($encode_filter_name, array(), $item, $data);
+
+			return $this->output_success($encoded_item);
+		}
+
+		public static function test_import() {
+			echo("Imported \OddCore\RestApi\CustomItemEndpoint<br />");
+		}
+	}
+?>

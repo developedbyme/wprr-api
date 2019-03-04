265eb69aa2de39198d3eaddc403ef088b49a8d0c
Mattias Ekendahl <mattias@developedbyme.com>
2019-03-04 11:52:28 +0100

    Update: action to start a subscription

--- libs/Wprr/ApiActionHooks.php
+++ libs/Wprr/ApiActionHooks.php
@@ -16,6 +16,8 @@
 			add_action('wprr/api_action/woocommerce/apply-dicount-code', array($this, 'hook_woocommerce_apply_discount_code'), 10, 2);
 			add_action('wprr/api_action/woocommerce/checkout', array($this, 'hook_woocommerce_checkout'), 10, 2);
 			add_action('wprr/api_action/woocommerce/empty-cart', array($this, 'hook_woocommerce_empty_cart'), 10, 2);
+			
+			add_action('wprr/api_action/woocommerce/subscriptions/start-subscription', array($this, 'hook_woocommerce_subscriptions_start_subscription'), 10, 2);
 
 		}
 
@@ -63,7 +65,7 @@
 			$order = wc_get_order( $order_id );
 			//update_post_meta($order_id, '_customer_user', get_current_user_id());
 			$order->calculate_totals();
-			$response_data['order_id'] = $order_id;
+			$response_data['orderId'] = $order_id;
 			
 			//Empty cart
 			WC()->cart->empty_cart();
@@ -73,6 +75,59 @@
 			//echo("\Wprr\ApiActionHooks::hook_woocommerce_empty_cart<br />");
 			
 			WC()->cart->empty_cart();
+		}
+		
+		public function hook_woocommerce_subscriptions_start_subscription($data, &$response_data) {
+			
+			//METODO: check that the current user is the owner of the subscription
+			
+			$order_id = $data['orderId'];
+			$start_date = $data['startDate'];
+			
+			$order = wc_get_order($order_id);
+			
+			$subscription_ids = array();
+			
+			$subscription_groups = array();
+			
+			foreach($order->get_items() as $item_id => $item_data) {
+				$product = $item_data->get_product();
+				$product_id = $product->get_id();
+				
+				$period = \WC_Subscriptions_Product::get_period( $product_id );
+				$interval = \WC_Subscriptions_Product::get_interval( $product_id );
+				
+				$group_name = $period.'_'.$interval;
+				if(!isset($subscription_groups[$group_name])) {
+					$subscription_groups[$group_name] = array(
+						'period' => $period,
+						'interval' => $interval,
+						'items' => array()
+					);
+				}
+				
+				$subscription_groups[$group_name]['items'][] = $item_data;
+			}
+			
+			foreach($subscription_groups as $group_name => $group_data) {
+				$subscription = wcs_create_subscription(array('order_id' => $order_id, 'billing_period' => $group_data['period'], 'billing_interval' => $group_data['interval'], 'start_date' => $start_date));
+				
+				foreach($group_data['items'] as $item_data) {
+					$product = $item_data->get_product();
+					$product_id = $product->get_id();
+					
+					//METODO: add meta
+					
+					$subscription->add_product($product, $item_data->get_quantity(), array());
+				}
+				
+				$subscription->calculate_totals();
+				$subscription_ids[] = $subscription->get_id();
+			}
+			
+			$response_data['subscriptionIds'] = $subscription_ids;
+			
+			\WC_Subscriptions_Manager::activate_subscriptions_for_order($order);
 		}
 		
 		public static function test_import() {

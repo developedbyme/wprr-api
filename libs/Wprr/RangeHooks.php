0fcd82e02aac2c7e49e291c199fb7294a538bf27
Mattias Ekendahl <mattias@developedbyme.com>
2019-03-31 21:33:02 +0200

    Update: added taxonomy query

--- libs/Wprr/RangeHooks.php
+++ libs/Wprr/RangeHooks.php
@@ -12,6 +12,45 @@
 			
 		}
 		
+		protected function add_tax_query(&$query_args, $tax_query, $relation = 'AND') {
+			if(isset($query_args['tax_query'])) {
+				$combined_query = array(
+					'relation' => $relation,
+					$tax_query,
+					$query_args['tax_query']
+				);
+				
+				$query_args['tax_query'] = $combined_query;
+			}
+			else {
+				$query_args['tax_query'] = array($tax_query);
+			}
+			
+			return $query_args;
+		}
+		
+		protected function get_term_ids($data) {
+			$return_array = array();
+			
+			$terms = explode(',', $data['terms']);
+			$termsField = isset($data['termsField']) ? $data['termsField'] : 'slugPath';
+			
+			foreach($terms as $term) {
+				if($termsField === 'slugPath') {
+					$current_term = \Wprr\OddCore\Utils\TaxonomyFunctions::get_term_by_slug_path($term, $data['taxonomy']);
+				}
+				else {
+					$current_term = get_term_by($termsField, $term, $data['taxonomy']);
+				}
+				
+				if($current_term) {
+					$return_array[] = $current_term->term_id;
+				}
+			}
+			
+			return $return_array;
+		}
+		
 		protected function register_hook_for_type($type, $hook_name) {
 			
 		}
@@ -23,13 +62,16 @@
 			add_filter(WPRR_DOMAIN.'/range_query/default', array($this, 'filter_query_standard'), 10, 2);
 			//add_filter(WPRR_DOMAIN.'/range_selection_has_permission/drafts', array('\Wprr\PermissionFilters', 'waterfall_is_admin'), 10, 1);
 			add_filter(WPRR_DOMAIN.'/range_query/drafts', array($this, 'filter_query_drafts'), 10, 2);
+			add_filter(WPRR_DOMAIN.'/range_query/attachmentStatus', array($this, 'filter_query_attachment_status'), 10, 2);
 			add_filter(WPRR_DOMAIN.'/range_query/idSelection', array($this, 'filter_query_id_selection'), 10, 2);
+			add_filter(WPRR_DOMAIN.'/range_query/inTaxonomy', array($this, 'filter_query_in_taxonomy'), 10, 2);
 			
 			add_filter(WPRR_DOMAIN.'/range_encoding/id', array($this, 'filter_encode_id'), 10, 3);
 			add_filter(WPRR_DOMAIN.'/range_encoding/standard', array($this, 'filter_encode_standard'), 10, 3);
 			add_filter(WPRR_DOMAIN.'/range_encoding/default', array($this, 'filter_encode_standard'), 10, 3);
 			add_filter(WPRR_DOMAIN.'/range_encoding/status', array($this, 'filter_encode_status'), 10, 3);
 			add_filter(WPRR_DOMAIN.'/range_encoding/translations', array($this, 'filter_encode_translations'), 10, 3);
+			add_filter(WPRR_DOMAIN.'/range_encoding/attachment', array($this, 'filter_encode_attachment'), 10, 3);
 			
 			add_filter(WPRR_DOMAIN.'/range_encoding/editFields', array($this, 'filter_encode_standard'), 10, 3);
 			add_filter(WPRR_DOMAIN.'/range_encoding/editFields', array($this, 'filter_encode_edit_fields'), 10, 3);
@@ -54,6 +96,22 @@
 			return $query_args;
 		}
 		
+		public function filter_query_in_taxonomy($query_args, $data) {
+			//echo("\Wprr\RangeHooks::filter_query_in_taxonomy<br />");
+			
+			$term_ids = $this->get_term_ids($data);
+			
+			$current_tax_query = array(
+				'taxonomy' => $data['taxonomy'],
+				'field' => 'id',
+				'terms' => $term_ids,
+				'include_children' => (($data['includeTermChildren'] == '1') ? true : false)
+			);
+			$this->add_tax_query($query_args, $current_tax_query);
+			
+			return $query_args;
+		}
+		
 		public function filter_query_drafts($query_args, $data) {
 			//echo("\Wprr\RangeHooks::filter_query_drafts<br />");
 			
@@ -63,6 +121,18 @@
 			
 			$query_args['post_status'][] = 'draft';
 			$query_args['post_status'][] = 'pending';
+			
+			return $query_args;
+		}
+		
+		public function filter_query_attachment_status($query_args, $data) {
+			//echo("\Wprr\RangeHooks::filter_query_attachment_status<br />");
+			
+			if(!isset($query_args['post_status'])) {
+				$query_args['post_status'] = array('publish');
+			}
+			
+			$query_args['post_status'][] = 'inherit';
 			
 			return $query_args;
 		}
@@ -102,6 +172,13 @@
 			$encoded_data['status'] = get_post_status($post_id);
 			
 			return $encoded_data;
+		}
+		
+		public function filter_encode_attachment($encoded_data, $post_id, $data) {
+			//echo("\Wprr\RangeHooks::filter_encode_attachment<br />");
+			$encoder = new \Wprr\WprrEncoder();
+			
+			return $encoder->encode_image(get_post($post_id));
 		}
 		
 		public function filter_encode_translations($encoded_data, $post_id, $data) {

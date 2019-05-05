3f386def8026c80be9f6616bafdd333bf8c8bb27
Mattias Ekendahl <mattias@developedbyme.com>
2019-05-05 22:00:40 +0200

    Update: selection of orders and subscriptions

--- libs/Wprr/RangeHooks.php
+++ libs/Wprr/RangeHooks.php
@@ -69,6 +69,10 @@
 			add_filter(WPRR_DOMAIN.'/range_filter/myOrders', array($this, 'filter_filter_my_orders'), 10, 2);
 			add_filter(WPRR_DOMAIN.'/range_query/inTaxonomy', array($this, 'filter_query_in_taxonomy'), 10, 2);
 			
+			add_filter(WPRR_DOMAIN.'/range_query/allOrderStatuses', array($this, 'filter_query_allOrderStatuses'), 10, 2);
+			add_filter(WPRR_DOMAIN.'/range_query/allSubscriptionStatuses', array($this, 'filter_query_allSubscriptionStatuses'), 10, 2);
+			add_filter(WPRR_DOMAIN.'/range_query/activeSubscriptions', array($this, 'filter_query_activeSubscriptions'), 10, 2);
+			
 			add_filter(WPRR_DOMAIN.'/range_encoding/id', array($this, 'filter_encode_id'), 10, 3);
 			add_filter(WPRR_DOMAIN.'/range_encoding/standard', array($this, 'filter_encode_standard'), 10, 3);
 			add_filter(WPRR_DOMAIN.'/range_encoding/default', array($this, 'filter_encode_standard'), 10, 3);
@@ -81,6 +85,10 @@
 			add_filter(WPRR_DOMAIN.'/range_encoding/editFields', array($this, 'filter_encode_status'), 10, 3);
 			
 			add_filter(WPRR_DOMAIN.'/range_encoding/fullPost', array($this, 'filter_encode_full_post'), 10, 3);
+			
+			add_filter(WPRR_DOMAIN.'/range_encoding/order', array($this, 'filter_encode_order'), 10, 3);
+			add_filter(WPRR_DOMAIN.'/range_encoding/subscription', array($this, 'filter_encode_order'), 10, 3);
+			add_filter(WPRR_DOMAIN.'/range_encoding/subscription', array($this, 'filter_encode_subscription'), 10, 3);
 		}
 		
 		public function filter_query_standard($query_args, $data) {
@@ -286,6 +294,124 @@
 			$encoded_data = mrouter_encode_post(get_post($post_id));
 			
 			return $encoded_data;
+		}
+		
+		public function filter_encode_order($return_object, $post_id) {
+			
+			$order = new \WC_Order($post_id);
+			
+			$return_object['status'] = $order->get_status();
+			$return_object['currency'] = $order->get_currency();
+			$return_object['date'] = $order->get_date_created()->date('Y-m-d H:i:s');
+			$return_object['user'] = wprr_encode_user($order->get_user());
+			$return_object['coupons'] = $order->get_used_coupons();
+			$return_object['totals'] = array(
+				'total' => $order->get_total(),
+				'discount_total' => $order->get_discount_total()
+			);
+			$return_object['taxTotals'] = $order->get_tax_totals();
+			
+			$billing_attributes = array('first_name', 'last_name', 'address_1', 'address_2', 'postcode', 'city', 'country', 'email', 'phone');
+			$billing_details = array();
+			foreach($billing_attributes as $billing_attribute) {
+				$get_function_name = 'get_billing_'.$billing_attribute;
+				$billing_details[$billing_attribute] = $order->$get_function_name();
+			}
+			$return_object['contactDetails'] = array('billing' => $billing_details);
+			
+			$line_items = $order->get_items();
+			
+			$encoded_items = array();
+			foreach($line_items as $line_item) {
+				$encoded_item = array(
+					'quantity' => $line_item->get_quantity(),
+					'product' => mrouter_encode_post_link($line_item->get_product_id()),
+					'total' => $line_item->get_total()
+				);
+				
+				$encoded_items[] = $encoded_item;
+			}
+			
+			$return_object['items'] = $encoded_items;
+			
+			return $return_object;
+		}
+		
+		public function filter_query_allOrderStatuses($query_args, $data) {
+			
+			$current_user_id = get_current_user_id();
+			
+			if(!current_user_can('edit_others_posts')) {
+				$query_args['post__in'] = array(0);
+				return $query_args;
+			}
+			
+			$query_args['post_status'] = array_keys( wc_get_order_statuses() );
+			
+			return $query_args;
+		}
+		
+		public function filter_query_allSubscriptionStatuses($query_args, $data) {
+			//echo("\Wprr\RangeHooks::query_allSubscriptionStatuses<br />");
+			
+			$current_user_id = get_current_user_id();
+			
+			if(!current_user_can('edit_others_posts')) {
+				$query_args['post__in'] = array(0);
+				return $query_args;
+			}
+			
+			$query_args['post_status'] = array( 'wc-pending', 'wc-active', 'wc-on-hold', 'wc-pending-cancel', 'wc-cancelled', 'wc-expired' );
+			
+			return $query_args;
+		}
+		
+		public function filter_query_activeSubscriptions($query_args, $data) {
+			//echo("\Wprr\RangeHooks::filter_query_activeSubscriptions<br />");
+			
+			$current_user_id = get_current_user_id();
+			
+			if(!current_user_can('edit_others_posts')) {
+				$query_args['post__in'] = array(0);
+				return $query_args;
+			}
+			
+			$query_args['post_status'] = array('wc-active');
+			
+			return $query_args;
+		}
+		
+		public function filter_encode_subscription($return_object, $post_id) {
+			
+			$subscription = new \WC_Subscription($post_id);
+			
+			$related_order_ids = $subscription->get_related_orders();
+			
+			$encoded_orders = array();
+			foreach($related_order_ids as $related_order_id) {
+				$order = new \WC_Order($related_order_id);
+				$encoded_order = array();
+				$encoded_order['id'] = $related_order_id;
+				$encoded_order['status'] = $order->get_status();
+				$encoded_order['total'] = $order->get_total();
+				$encoded_order['currency'] = $order->get_currency();
+				$encoded_order['date'] = $order->get_date_created()->date('Y-m-d H:i:s');
+				
+				$encoded_orders[] = $encoded_order;
+			}
+			$return_object['relatedOrders'] = $encoded_orders;
+			
+			$date_types = array('start', 'trial_end', 'next_payment', 'last_payment', 'end');
+			$encoded_dates = array();
+			foreach($date_types as $date_type) {
+				$encoded_dates[$date_type] = $subscription->get_date($date_type);
+				if($encoded_dates[$date_type] === 0) {
+					$encoded_dates[$date_type] = null;
+				}
+			}
+			$return_object['dates'] = $encoded_dates;
+			
+			return $return_object;
 		}
 		
 		public static function test_import() {

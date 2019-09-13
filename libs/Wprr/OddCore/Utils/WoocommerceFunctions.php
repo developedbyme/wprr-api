3b044b6dded241d78ac413ca0b4ef678f1388601
Mattias Ekendahl <mattias@developedbyme.com>
2019-09-13 10:53:45 +0200

    Update: checking if cart exists before loading new

--- libs/Wprr/OddCore/Utils/WoocommerceFunctions.php
+++ libs/Wprr/OddCore/Utils/WoocommerceFunctions.php
@@ -6,33 +6,35 @@
 		
 		public static function ensure_wc_has_cart() {
 			
-			if ( defined( 'WC_ABSPATH' ) ) {
-				// WC 3.6+ - Cart and notice functions are not included during a REST request.
-				include_once WC_ABSPATH . 'includes/wc-cart-functions.php';
-				include_once WC_ABSPATH . 'includes/wc-notice-functions.php';
-			}
-
-			if ( null === WC()->session ) {
-				$session_class = apply_filters( 'woocommerce_session_handler', 'WC_Session_Handler' );
-
-				//Prefix session class with global namespace if not already namespaced
-				if ( false === strpos( $session_class, '\\' ) ) {
-					$session_class = '\\' . $session_class;
+			if ( null === WC()->cart ) {
+				if ( defined( 'WC_ABSPATH' ) ) {
+					// WC 3.6+ - Cart and notice functions are not included during a REST request.
+					include_once WC_ABSPATH . 'includes/wc-cart-functions.php';
+					include_once WC_ABSPATH . 'includes/wc-notice-functions.php';
 				}
 
-				WC()->session = new $session_class();
-				WC()->session->init();
-			}
+				if ( null === WC()->session ) {
+					$session_class = apply_filters( 'woocommerce_session_handler', 'WC_Session_Handler' );
 
-			if ( null === WC()->customer ) {
-				WC()->customer = new \WC_Customer( get_current_user_id(), true );
-			}
+					//Prefix session class with global namespace if not already namespaced
+					if ( false === strpos( $session_class, '\\' ) ) {
+						$session_class = '\\' . $session_class;
+					}
 
-			if ( null === WC()->cart ) {
-				WC()->cart = new \WC_Cart();
+					WC()->session = new $session_class();
+					WC()->session->init();
+				}
 
-				// We need to force a refresh of the cart contents from session here (cart contents are normally refreshed on wp_loaded, which has already happened by this point).
-				WC()->cart->get_cart();
+				if ( null === WC()->customer ) {
+					WC()->customer = new \WC_Customer( get_current_user_id(), true );
+				}
+
+				if ( null === WC()->cart ) {
+					WC()->cart = new \WC_Cart();
+
+					// We need to force a refresh of the cart contents from session here (cart contents are normally refreshed on wp_loaded, which has already happened by this point).
+					WC()->cart->get_cart();
+				}
 			}
 		}
 		

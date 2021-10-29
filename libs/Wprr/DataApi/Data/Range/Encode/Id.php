469a58963a8e7a0ffba90fd6d1e759340c7ff921
Mattias Ekendahl <mattias@developedbyme.com>
2021-10-29 08:52:09 +0200

    Update: idselection for data api

---
+++ libs/Wprr/DataApi/Data/Range/Encode/Id.php
@@ -0,0 +1,20 @@
+<?php
+	namespace Wprr\DataApi\Data\Range\Encode;
+
+	// \Wprr\DataApi\Data\Range\Encode\Id
+	class Id {
+
+		function __construct() {
+			
+		}
+		
+		public function encode($id) {
+			//var_dump("Id::encode");
+			
+		}
+
+		public static function test_import() {
+			echo("Imported \Wprr\DataApi\Id<br />");
+		}
+	}
+?>
---
+++ libs/Wprr/DataApi/Data/Range/Select/IdSelection.php
@@ -0,0 +1,36 @@
+<?php
+	namespace Wprr\DataApi\Data\Range\Select;
+
+	// \Wprr\DataApi\Data\Range\Select\IdSelection
+	class IdSelection {
+
+		function __construct() {
+			
+		}
+		
+		public function select($query, $data) {
+			//var_dump("IdSelection::select");
+			
+			global $wprr_data_api;
+			
+			$has_query = false;
+			
+			if(!isset($data['ids'])) {
+				//METODO: throw
+			}
+			
+			$ids = array_map(function($value) {return (int)$value;}, explode(',', $data['ids']));
+			$query->include_only($ids);
+		}
+		
+		public function filter($posts, $data) {
+			//var_dump("IdSelection::filter");
+			
+			return $posts;
+		}
+
+		public static function test_import() {
+			echo("Imported \Wprr\DataApi\IdSelection<br />");
+		}
+	}
+?>

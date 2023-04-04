411000f2a7208dc6fa3400f17afbbb743c4b8bac
Mattias Ekendahl <mattias@developedbyme.com>
2023-04-04 09:52:12 +0200

    Update: performance for ensure cart

--- libs/Wprr/OddCore/Utils/WoocommerceFunctions.php
+++ libs/Wprr/OddCore/Utils/WoocommerceFunctions.php
@@ -7,11 +7,15 @@
 		public static function ensure_wc_has_cart() {
 			//echo('\Wprr\OddCore\Utils\WoocommerceFunctions::ensure_wc_has_cart');
 			
+			wprr_performance_tracker()->start_meassure('WoocommerceFunctions::ensure_wc_has_cart');
+			
 			include_once WC_ABSPATH . 'includes/wc-cart-functions.php';
 			include_once WC_ABSPATH . 'includes/wc-notice-functions.php';
 			wc_load_cart();
 			WC()->cart->get_cart();
 			WC()->cart->calculate_totals();
+			
+			wprr_performance_tracker()->stop_meassure('WoocommerceFunctions::ensure_wc_has_cart');
 		}
 		
 		public static function test_import() {
